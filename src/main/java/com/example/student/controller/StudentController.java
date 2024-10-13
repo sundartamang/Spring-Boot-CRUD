d78628a6e61764f1a91cfafbda8524e0fa59fa14
sursasundar <sundar.tamang@sursatech.com>
2024-10-13 11:43:01 +0545

    update application properties, implmennted service interface

--- src/main/java/com/example/student/controller/StudentController.java
+++ src/main/java/com/example/student/controller/StudentController.java
@@ -1,4 +1,12 @@
 package com.example.student.controller;
 
+import com.example.student.service.StudentService;
+
 public class StudentController {
+    private final StudentService studentService;
+
+    StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
 }
--- src/main/java/com/example/student/service/StudentService.java
+++ src/main/java/com/example/student/service/StudentService.java
@@ -1,4 +1,41 @@
 package com.example.student.service;
 
-public class StudentService {
+import com.example.student.model.Student;
+import com.example.student.repository.StudentRepository;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+public class StudentService implements IStudentService {
+    private final StudentRepository studentRepository;
+
+    StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return List.of();
+    }
+
+    @Override
+    public Student getStudentById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public void deleteStudentById(Integer id) {
+
+    }
+
+    @Override
+    public Student SaveStudent(Student student, MultipartFile file) throws IOException {
+        return null;
+    }
+
+    @Override
+    public Student updateStudentById(Long studentId, Student student, MultipartFile file) throws IOException {
+        return null;
+    }
 }
